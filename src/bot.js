b489ee1f5adefee3e685cfbc96967a90f8b263a1
Fabio Picheli <contato@fabiopicheli.com>
2017-12-12 16:19:54 +0100

    fix(): fix rft label

--- src/bot.js
+++ src/bot.js
@@ -80,7 +80,7 @@ class Bot {
         const rejected = resp.filter(item => item.state === 'CHANGES_REQUESTED');
         const approved = resp.filter(item => item.state === 'APPROVED');
         if (rejected.length === 0 && approved.length >= config.github.reviewsNeeded) {
-          this.addLabels(pr, config.github.label.ready, callback);
+          this.addLabels(pr, [config.github.label.ready], callback);
         }
 
         // TODO: Trigger Jira
