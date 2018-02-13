c4b543ce05097b8efba2ed0f34e1fa2eb03a4f71
Fabio Picheli <contato@fabiopicheli.com>
2018-02-13 12:38:26 +0100

    add heroku webhook

--- src/routes/heroku.js
+++ src/routes/heroku.js
@@ -4,12 +4,7 @@ const router = express.Router();
 let log = [];
 
 router.post('/', function (req, res) {
-  log.push(req.body);
   res.json({ status: 'Up and running ' });
-});
-
-router.get('/log', function (req, res) {
-  res.json({ log });
 });
 
 module.exports = router;
