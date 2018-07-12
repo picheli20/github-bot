3c0392f428af29258a1817d9c9bb35f4a7e8089c
XCaliber CI <intellix+ci@gmail.com>
2018-07-12 13:49:54 +0200

    Update screenshotUrl

--- internal/config/index.ts
+++ internal/config/index.ts
@@ -12,7 +12,7 @@ interface IConfig {
 }
 
 export const config: IConfig = {
-  screenshotUrl: 'http://ec2-18-197-175-232.eu-central-1.compute.amazonaws.com',
+  screenshotUrl: 'http://18.185.247.217',
   jira: {
     auth: pe.JIRA_AUTH || '',
     server: pe.JIRA_SERVER || '',
